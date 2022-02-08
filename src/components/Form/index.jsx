1fae501f28bac2cd0cca7d208f2b4063459c1c9f
Guilherme Augusto da Silva <guilhermeshadow91@gmail.com>
2022-02-07 21:04:30 -0300

    CSS almost completed, Logic in progess

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -22,14 +22,16 @@ const Form = ({moldTransaction}) => {
         <form onSubmit={prevent}>
             <div className="form">
                 <div className="inputDescription">
+                    <h3>Descrição</h3>
                     <input onChange={(event) => setDescription(event.target.value)} type="text" placeholder="Digite aqui sua descrição"></input>
+                    <p>Ex: Compra de roupas</p>
                 </div>    
                 <div className="inputValue">
                     <input onChange={(event) => setValue(Number(event.target.value))} type="number" placeholder="Valor R$"></input>
                     <select className="inputsTypes" onClick={validateType}>
                     <option value="entrada">Entrada</option>
                     <option value="saída">Saída</option>
-            </select>
+                    </select>
                 </div>
             <button className="buttonForm" onClick={() => moldTransaction(obj)}>Inserir</button>
             </div>
